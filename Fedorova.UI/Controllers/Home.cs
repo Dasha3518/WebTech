9f941719c9d4fde7827e8fb266fad21f41d103ee
Fedorova Darya <fedorovadarya3518@outlook.com>
2025-06-29 12:31:32 +0300

    ЛБ2

--- Fedorova.UI/Controllers/Home.cs
+++ Fedorova.UI/Controllers/Home.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fedorova.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Fedorova.UI.Controllers
+namespace WebLabs.Controllers
 {
     public class Home : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            ViewData["Title"] = "Лабораторная работа №2";
+            ViewData["HeaderText"] = "Лабораторная работа №2";
+
+            var items = new List<ListDemo>() {
+            new ListDemo() { Id = 1, Name = "Item 1" },
+            new ListDemo() { Id = 2, Name = "Item 2" },
+            new ListDemo() { Id = 3, Name = "Item 3" }
+            };
+
+            return View(items);
         }
     }
+
 }
